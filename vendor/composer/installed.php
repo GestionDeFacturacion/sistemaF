eadbb1af5c1ba187a2193d4adf68df1927d15261
Gian-windr <gcamposa2@upao.edu.pe>
2026-01-18 17:12:14 -0500

    Prueba

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '515a47795f950dad69dd95a02b54383b917cc54c',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -611,9 +611,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '515a47795f950dad69dd95a02b54383b917cc54c',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
